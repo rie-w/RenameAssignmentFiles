5297a7601f2f775762b879af24b143a860a98c86
rie-w@nttr.co.jp <rie-w@sectool-10-pro-vulsdb-001.ms.internal>
2018-02-21 17:24:13 +0900

    変数名を変えた

--- rename_assignment.bat
+++ rename_assignment.bat
@@ -1,18 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set 拡張子=""
-set 新しいファイル名=""
+set EXTENTION=""
+set NEW_FILE_NAME=""
 
 IF exist ReadMe.csv (
   FOR /F "tokens=1-3 delims=," %%i in (ReadMe.csv) do (
-    FOR /F "tokens=1-3 delims=." %%A in ("%%k") do set 拡張子=%%B
+    FOR /F "tokens=1-3 delims=." %%A in ("%%k") do set EXTENTION=%%B
     IF exist %%k (
-      set 新しいファイル名=%%j.!拡張子!
-      ren %%k !新しいファイル名!
+      set NEW_FILE_NAME=%%j.!EXTENTION!
+      ren %%k !NEW_FILE_NAME!
     )
   )
 ) else (
-  echo このファイルを、アサインメントのzipを解凍したフォルダにおいて実行すると、ファイル名が変更されます。
+  echo "Please put this file on assignment folder"
 )
 pause
