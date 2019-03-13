0f00402357065e5737213edaef70b50a90768200
Watanabe Rie <rie>
2019-03-13 12:22:16 +0900

    ファイル名の先頭にクラスとDayを表示するように修正

--- rename.bat
+++ rename.bat
@@ -2,14 +2,29 @@
 setlocal enabledelayedexpansion
 
 set EXTENTION=""
+set _FILE_NAME=""
+set FILE_NAME=""
+set CLASS=""
+set DAY_NUM=""
 set NEW_FILE_NAME=""
 
 IF exist ReadMe.csv (
   FOR /F "tokens=1-3 delims=," %%i in (ReadMe.csv) do (
-    FOR /F "tokens=1-3 delims=." %%A in ("%%k") do set EXTENTION=%%B
-    IF exist %%k (
-      set NEW_FILE_NAME=%%j.!EXTENTION!
-      ren %%k !NEW_FILE_NAME!
+    FOR /F "tokens=1-3 delims=." %%A in ("%%k") do (
+
+      set EXTENTION=%%B
+      set _FILE_NAME=%%A
+
+      FOR /F "tokens=1-3 delims=_" %%a in ("%%A") do (
+          set  CLASS=%%a
+          set  DAY_NUM=%%b
+      )
+      
+      IF exist %%k (
+       set NEW_FILE_NAME=!CLASS!_!DAY_NUM!_%%j.!EXTENTION!
+        ren %%k !NEW_FILE_NAME!
+      )
+
     )
   )
 ) else (
